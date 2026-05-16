5486b820aa023c949754efc64e4a76429c079a27
Kasharid09 <denskt1@students.fscj.edu>
2026-05-16 13:20:41 -0400

    Add files via upload
    Introduction to PalmerPenguins

---
+++ PalmerPenguins.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.palmerpenguins;
+
+//PalmerPenguins.java
+//Kashari Denson
+// 05/15/2026
+// Introduction to the PalmerPenguins
+
+public class PalmerPenguins {
+    
+     // STATIC FINAL VARIABLES 
+        static final String SP_CHINSTRAP = "Chinstrap";
+        static final String SP_GENTOO = "Gentoo";
+        static final String SP_ADELIE = "Adelie";
+        static final int TOTAL_SPECIES = 3;
+        
+        
+    public static void main(String[] args) {
+        
+        // OUTPUT THE SPECIES NAMES WITH INTRODUCTORY TEXT
+        System.out.println("Introducing the Palmer Penguins: ");
+        System.out.println("\t" + SP_CHINSTRAP + "!");
+        System.out.println("\t" + SP_GENTOO  + "!");
+        System.out.println("and last but not least...");
+        System.out.println("\t" + SP_ADELIE + "!");
+        System.out.println("There are a total of " + TOTAL_SPECIES + 
+                            " penguin species in this dataset.");
+        
+       
+        
+
+        
+        
+        
+        
+    }
+}
